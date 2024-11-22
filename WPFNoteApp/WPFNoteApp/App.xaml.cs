a31a1b1dcd29ae0d798245ac087b1fc149378f29
chinhminh <chinhminh2003@gmail.com>
2024-11-22 22:56:32 +0700

    UI for Note

---
+++ WPFNoteApp/WPFNoteApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPFNoteApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
